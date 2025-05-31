049e63ea9d8400350c95e9e2b9c707c7d02336f2
Trevor <trevorward001@gmail.com>
2025-05-30 19:57:36 -0600

    fix: keep tree to left

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -16,12 +16,20 @@ return {
         },
   },
       filesystem = {
-        hijack_netrw_behavior = "open_default", 
         follow_current_file = {
           enabled = true, -- This will find and focus the file in the active buffer every time
           leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
       },
       close_if_last_window = true,
       }
+  },
+  event_handlers = {
+    {
+      event = "neo_tree_window_after_open",
+      handler = function(args)
+        -- Force Neo-tree to always stay at the far left
+        vim.cmd("wincmd H")
+      end,
+    },
   },
 }
